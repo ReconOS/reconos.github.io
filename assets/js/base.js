2643cb41896f3eb93cb00809ab41c87dcb919044
Christoph Rüthing <ruething@mail.upb.de>
2014-03-31 15:20:55 +0200

    Added javascript to add captions to images

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,8 +1,16 @@
 // JavaScript Document
 
-function initPage() {
+// Create captions for images in the content section
+// Wrap them by a div to control margins
+function createImageCaptions() {
+	$('#content img').each(function() {
+		$(this).wrap('<div class="figure"></div>')
+		caption = $(this).attr('alt');
+		if (caption !== "")
+			$(this).after('<p>' + caption + '</p>');
+	});
 }
 
 $(document).ready(function(e) {
-    initPage();
+	createImageCaptions();
 });
